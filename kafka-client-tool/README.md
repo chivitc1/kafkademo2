e96d4ae2b40eda03ff56258685045f93eb7f98da
chivitc1 <chivitc1@gmail.com>
2018-11-23 17:24:54 +0700

    Restructure project include kafka-scs-tool and kafka-client-tool

---
+++ kafka-client-tool/README.md
@@ -0,0 +1,1 @@
+Kafka client auth demo
---
+++ kafka-scs-tool/src/main/java/chinv/nal/MainApp.java
@@ -0,0 +1,26 @@
+package chinv.nal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chi.nguyen
+ */
+@SpringBootApplication
+@Slf4j
+public class MainApp implements CommandLineRunner
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MainApp.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception
+	{
+		String str = System.getProperty("kafka.user.name");
+		log.info("JVM user name: " + str);
+	}
+}
